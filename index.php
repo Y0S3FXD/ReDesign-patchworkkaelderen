32408e6f687fc725528d43e8952826afcf2e15f8
Y0S3FXD <shekyousef@gmail.com>
2024-02-20 13:04:07 +0100

    SETUP

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patchwork Kælderen</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh21S3ygy58CTewzR9TThjxyu9zUCe05ORuTA0mE14hGgX2e2EaMDx/VhVTg" crossorigin="anonymous">
+    <link rel="stylesheet"  href="style.css">
+</head>
+
+<body>
+    <header class="container-fluid py-3 bg-dark text-light">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-2 d-flex align-items-center">
+                    <img src="logo.png" alt="Patchwork Kælderen logo" class="img-fluid" style="max-width: 100px;">
+                </div>
+                <div class="col-6 text-start">
+                    <h1 class="display-5 fw-bold text-uppercase">Patchwork Kælderen</h1>
+                </div>
+                <div class="col-4 text-end">
+                    <p class="lead">Forretning - Tilbehør - Symaskiner - Quiltecafé - Quiltning</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <nav class="container-fluid bg-light">
+        <div class="container">
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Hjem</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Om os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Produkter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kurser</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <main class="container py-5">
+        <div class="row">
+            <div class="col-md-8">
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id ligula porta felis euismod semper. Nulla vitae libero pulvinar, eget leo quis, ornare tortor. Donec ullamcorper nulla non metus auctor fringilla. Sed posuere consectetur est at lobortis.</p>
+                <p>Mauris blandit aliquam erat, eget tincidunt dui lobortis eget. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                <a href="#" class="btn btn-primary">Læs mere</a>
+            </div>
+            <div class="col-md-4">
+                <aside>
+                    <h2>Åbningstider</h2>
+                    <ul>
+                        <li>Tirsdag, torsdag og fredag: 12.00-17.00</li>
+                        <li>Lørdag: Efter aftale</li>
+                    </ul>
+                    <h2>Adresse</h2>
+                    <p>Læsseegade 47, 6000 Kolding</p>
+                    <h2>Kontakt</h2>
+                    <p>
+                        E-mail: info@patchworkkaelderen.dk<br>
+                        Telefon: +45 22266277
+                    </p>
+                </aside>
+            </div>
+        </div>
+    </main>
+
+    <footer class="container-fluid bg-dark text-light py-3">
+        <div class="container
