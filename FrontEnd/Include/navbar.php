f5e21851ab857a8ec3645a4168b9764609c571d7
Casper <casp721b@mail.hansenberg.dk>
2024-02-27 12:51:48 +0100

    Navbar

--- FrontEnd/Include/navbar.php
+++ FrontEnd/Include/navbar.php
@@ -1,33 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">Patchwork Kælderen</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Patchwork Kælderen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Forretning</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="shop.php">Tilbehør</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Kørselsvejledning</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Symaskiner</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Quiltecafé</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Quiltning</a>
-                </li>
-            </ul>
-        </div>
-    </div>
+<nav class="navbar navbar-expand-sm bg-light justify-content-center">
+  <div class=navbarLogo>
+    <img src="Images/Logo.png">
+  </div>  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Hjem</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Kurser</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Produkter</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Quiltecafe</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Quiltning</a>
+    </li>    
+  </ul>
 </nav>
