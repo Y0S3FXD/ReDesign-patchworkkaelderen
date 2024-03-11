c1f6007ae27042870772e229e6837b19a399093a
Casper <casp721b@mail.hansenberg.dk>
2024-03-11 12:42:53 +0100

    Opdatering af footer

--- FrontEnd/Include/footer.php
+++ FrontEnd/Include/footer.php
@@ -4,7 +4,7 @@
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                 <h5>Patchwork Kælderen</h5>
                 <p>Læssøegade 47, 6000 Kolding
-                <br><a href="https://shorturl.at/HJKMW">Find vej</a></p>
+                <br><a href="https://shorturl.at/HJKMW" class="link-secondary">Find vej</a></p>
             </div>
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                 <h5>Kontakt os</h5>
@@ -13,6 +13,6 @@
         </div>
     </div>
     <div class="text-center p-3" style="background-color: rgba(129, 66, 13, 0.5);">
-        &copy; 2024 Patchwork Kælderen. Alle rettigheder forbeholdes.    
+        &copy; 2024 Patchwork Kælderen. Alle rettigheder forbeholdes. <br>Baggrundsbillede: <a href="https://denstoredanske.lex.dk/patchwork" class="link-secondary">Den Store Danske.</a> Billede til quiltning: <a href="https://pxhere.com/da/photo/148398" class="link-secondary">PxHere</a>.
     </div>
 </footer>
